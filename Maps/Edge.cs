c966da73aafda9c8db709cde62c2f1340a299382
Andrei Bucșa <andrei_raul04@yahoo.com>
2023-12-24 15:11:03 +0200

    bestroute

---
+++ Maps/Edge.cs
@@ -0,0 +1,19 @@
+﻿namespace Maps
+{
+    public class Edge
+    {
+        public Vertex start;
+        public Vertex end;
+        public double cost;
+
+        public Edge(string data)
+        {
+            string[] split = data.Split(' ');
+            start = Graph.FindVertice(int.Parse(split[0]));
+            end = Graph.FindVertice(int.Parse(split[1]));
+
+
+            cost = Graph.GetDistance(start.position, end.position);
+        }
+    }
+}
---
+++ Maps/PathWithCost.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Maps
+{
+    public class PathWithCost
+    {
+        public List<Vertex> Path;
+        public double cost;
+
+        public PathWithCost()
+        {
+            Path = new List<Vertex>();
+            cost = 0;
+        }
+    }
+}
